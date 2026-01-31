d1d278c85d9b1b4d3fc99654cf1a4769b4197e41
Ploythun <ploytwrsrijandon@gmail.com>
2026-01-31 23:38:21 +0700

    Lab15 complete

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,35 @@ int main(){
 }
 
 //Do not modify source code above this line
+void shuffle(int &a, int &b, int &c, int &d){
+	for(int i = 0; i < 10; i++){
+		int temp;
+		int x = rand()%6;
+
+		if(x == 0){
+			temp = a;
+			a = b;
+			b = temp;
+		}else if(x == 1){
+			temp = a;
+			a = c;
+			c = temp;
+		}else if(x == 2){
+			temp = a;
+			a = d;
+			d = temp;
+		}else if(x == 3){
+			temp = b;
+			b = c;
+			c = temp;
+		}else if(x == 4){
+			temp = b;
+			b = d;
+			d = temp;
+		}else {
+			temp = c;
+			c = d;
+			d = temp;
+		}
+	}
+}
--- lab15_5.cpp
+++ lab15_5.cpp
@@ -20,3 +20,14 @@ int main(){
 
 //Do not modify the code above this line
 //Write definition of shuffle() using pointer after this line. 
+void shuffle(int *A,int *B,int *C,int *D){
+	int *X[4] = {A,B,C,D}; //array ต้องเป็น int *X[] เพราะมันเก็บ pointer (A,B,C,D)
+	for(int k = 0; k < 10; k++){
+		int i = rand()%4;
+		int j = rand()%4;
+		if(i == j) continue;
+		int temp = *X[i]; //การเอาค่าที่ pointer ชี้ไป ต้องใช้ *X[i] ตอนเข้าถึงข้อมูล
+		*X[i] = *X[j];
+		*X[j] = temp;
+	}
+}
